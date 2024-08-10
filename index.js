700d64880e498b42140c3570a263c9a9e29ca9cf
Md. Likhon Mia <likhon15-3916@diu.edu.bd>
2024-08-10 16:04:38 -0700

    added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173', // Update this with your client URL
+    origin: 'https://visualized-dashboard.vercel.app', // Update this with your client URL
     allowedHeaders: ['Authorization', 'Content-Type'],
 }));
 
